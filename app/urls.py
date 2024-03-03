b89fc41f1ac84037cbe5afff15eeb48a3683608e
INAPAKRUTHIKASRILALITHACHANDANA <inpakruthikasrilalithachandana@gmail.com>
2024-03-03 10:40:17 +0500

    first commit

---
+++ app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from app import views
+
+
+urlpatterns = [
+    path('addTask/',views.addTask,name='addTask'),
+    path('mark_done/<int:pk>/',views.mark_done,name='mark_done'),
+    path('mark_notdone/<int:pk>/',views.mark_notdone,name='mark_notdone'),
+    path('edit/<int:pk>/',views.edit,name="edit"),
+    path('delete/<int:pk>/',views.delete,name="delete")
+]
